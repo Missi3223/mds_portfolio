b029f1866774ff6bd50184249a61935925d0c45f
Missi3223 <44407731+Missi3223@users.noreply.github.com>
2019-03-26 15:55:43 -0600

    Materialize and content working

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,8 +1,15 @@
-(function($){
-  $(function(){
+// On ready
+$(document).ready(function () {
+  // Floating Action Button - left - on click only
+  $(".fixed-action-btn").floatingActionButton({
+      direction: "left",
+      hoverEnabled: false
+  });
+  // Parallax call
+  $(".parallax").parallax();
+  // Sidenav call
+  $(".sidenav").sidenav();
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+});
+
